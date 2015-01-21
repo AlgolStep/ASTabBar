9b8ab2053f4e641e5aeb64ede8153c4501469dd4
思来氏 <omoeraishi@omoeraishis-iMac.local>
2015-01-21 13:46:34 +0800

    Initial Commit

---
+++ ASTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASTabBar
+//
+//  Created by 思来氏 on 15-1-21.
+//  Copyright (c) 2015年 PSYDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ASTabBar
+//
+//  Created by 思来氏 on 15-1-21.
+//  Copyright (c) 2015年 PSYDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
